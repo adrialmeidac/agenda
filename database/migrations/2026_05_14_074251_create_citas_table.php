3d33b69c08dc2785da01fca101514242ae864b2a
adrialmeidac <adrianaalmeidac@hotmail.com>
2026-05-14 09:48:59 +0200

    Modelo y migraciones

---
+++ database/migrations/2026_05_14_074251_create_citas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('citas', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo', 255);
+            $table->date('fecha');
+            $table->time('hora');
+            $table->string('descripcion', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('citas');
+    }
+};
